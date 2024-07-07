1c7d1b6a486b652331e3fd96f3f0d870dabffd55
Ryan Gordon <ryangordon@duck.com>
2024-07-07 21:52:59 +1200

    Ensure no repeat particles in combinations

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ PARTICLES = ["gas", "light", "gate", "keep", "girl", "boss"]
 if __name__ == "__main__":
     all_pairs = [p for p in itertools.permutations(PARTICLES, 2)]
 
-    all_combinations = [c for c in itertools.product(all_pairs, repeat=3)]
+    all_combinations = [c for c in itertools.permutations(all_pairs, 3)]
 
     for c in all_combinations:
-        print(f"{c[0][0]}{c[0][1]} {c[1][0]}{c[1][1]} {c[2][0]}{c[2][1]}")
+        if len(set([p for ps in c for p in ps])) == 6:
+            print(f"{c[0][0]}{c[0][1]} {c[1][0]}{c[1][1]} {c[2][0]}{c[2][1]}")
