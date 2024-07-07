3ef3de988573c09bc7ea9cc4d9db0098d50085be
Ryan Gordon <ryangordon@duck.com>
2024-07-07 23:25:17 +1200

    This is certainly code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use itertools::Itertools;
 use std::collections::HashSet;
+use rand::seq::SliceRandom;
+use std::io;
 
 const PARTICLES: [&str; 6] = ["gas", "light", "gate", "keep", "girl", "boss"];
 
@@ -10,10 +12,20 @@ fn main() {
         .filter(|i| i.iter().flatten().collect::<HashSet<_>>().len() == 6)
         .collect::<Vec<_>>();
 
-    println!("{:?}", combinations);
-    println!("{}", combinations.len());
+    loop {
+        let mut guess = String::new();
 
-    for c in combinations.iter() {
-        println!("{}{} {}{} {}{}", c[0][0], c[0][1], c[1][0], c[1][1], c[2][0], c[2][1]);
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+        println!(">>> {}", format_combination(random_combination(&combinations)));
     }
+}
+
+fn random_combination<'a>(combination: &'a Vec<Vec<Vec<&&'a str>>>) -> &'a Vec<Vec<&'a &'a str>> {
+    combination.choose(&mut rand::thread_rng()).unwrap()
+}
+
+fn format_combination(c: &Vec<Vec<&&str>>) -> String {
+    format!("{}{} {}{} {}{}", c[0][0], c[0][1], c[1][0], c[1][1], c[2][0], c[2][1])
 }
